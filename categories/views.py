ea0ed1fcc4ed6ba7deb7cf0d66dd84859dd85eb2
Rafiul29 <rafiulislam4472@gmail.com>
2024-05-31 00:51:01 +0600

    category data added into dbms

--- categories/views.py
+++ categories/views.py
@@ -1,8 +1,15 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from django.http import HttpResponse
 from . import forms
 # Create your views here.
 
 def add_category(request):
-  category_form=forms.CategoryForm()
+  if request.method=='POST':
+    category_form=forms.CategoryForm(request.POST)
+    if category_form.is_valid():
+      # print(category_form.cleaned_data)
+      category_form.save()
+      return redirect('add_task')
+  else:
+    category_form=forms.CategoryForm()
   return render(request,'add_category.html',{'form':category_form})
