b8c5b4acbba28f7733fb271b92b2638a9bf40e32
Rafiul29 <rafiulislam4472@gmail.com>
2024-05-31 00:38:05 +0600

    task and category form html render

--- categories/views.py
+++ categories/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from . import forms
 # Create your views here.
 
 def add_category(request):
-  return render(request,'add_category.html')
+  category_form=forms.CategoryForm()
+  return render(request,'add_category.html',{'form':category_form})
--- task/views.py
+++ task/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from . import forms
 # Create your views here.
 
 def add_task(request):
-  return render(request,'add_task.html')
+  task_form=forms.TaskForm()
+  return render(request,'add_task.html',{'form':task_form})
